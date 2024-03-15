043b6388a98fd3be42ab782fef96fa27dab741da
mehmet durmus <mdurmus@outlook.com>
2024-03-15 16:17:28 +0000

    fix: removed cabin_type on model

--- vehicle/admin.py
+++ vehicle/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from .models import Category, Vehicle, Comment, VehicleBrand, VehicleModel
 from django_summernote.admin import SummernoteModelAdmin
 
-@admin.register(Category)
-class CategoryAdmin(SummernoteModelAdmin):
-    summernote_fields =('title')
-
 @admin.register(Comment)
 class CommentAdmin(SummernoteModelAdmin):
     list_display = ('created_on',)
@@ -26,5 +22,7 @@ class VehicleAdmin(SummernoteModelAdmin):
     list_filter = ('status','created_on')
     summernote_fields=('summary','content')
 
+
 admin.site.register(VehicleBrand)
 admin.site.register(VehicleModel)
+admin.site.register(Category)
