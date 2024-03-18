5fbe464cec31d49f5a9e3a236b54d546c6416e1f
mehmet durmus <mdurmus@outlook.com>
2024-03-18 11:30:45 +0000

    navbar added search.html added and some edit completed

--- vehicle/views.py
+++ vehicle/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.views import generic, View 
+from .models import *
 
 class Home(generic.TemplateView):
     '''
     Home page view
     '''
     template_name='index.html'
+
+def search(request):
+    '''
+    search results
+    '''
+    # Get all vehicle data
+    queryset = Vehicle.objects.all()
+
+    if request.POST:
+        searched = request.POST["searched"]
+        results = Vehicle.objects.filter(Q(title__contains=searched) |
+                                     Q(brand__contains=searched) |
+                                     Q(content__contains=searched)
+                                    ).distinct()
+        context = {'queryset': queryset}
+
+        return render(request, 'vehicle/search.html', {'results': results, 'searched': searched})
+    else:
+        return render(request, 'vehicle/search.html', context)
+
+
