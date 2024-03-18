fc00233249787ba410ea71b868fdf1d8a8655d77
mehmet durmus <mdurmus@outlook.com>
2024-03-18 09:55:36 +0000

    created index, base html files and html parts files

--- vehicle/views.py
+++ vehicle/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic, View 
 
-# Create your views here.
+class Home(generic.TemplateView):
+    '''
+    Home page view
+    '''
+    template_name='index.html'
