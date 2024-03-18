96dc340718b78a14f883bfad2d21cdb33114b7f1
mehmet durmus <mdurmus@outlook.com>
2024-03-18 09:26:57 +0000

    added urls file to vehicle app

--- roadster_project/urls.py
+++ roadster_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('vehicle.urls'), name='vehicle-urls'),
     path('summernote/', include('django_summernote.urls')),
 ]
