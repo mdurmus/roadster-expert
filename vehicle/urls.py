4aa9471582dbc564478fa12477ad4fdf48c4a2dd
mehmet durmus <mdurmus@outlook.com>
2024-03-19 09:47:40 +0000

    cleared all files and updated all static files

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -3,7 +3,4 @@ from django.urls import path
 
 urlpatterns=[
     path('', views.Home.as_view(), name='home'),
-    path('search/', views.search, name="search"),
-    path('categories_posts/<str:cats', 
-         views.categories_view, name='vehicle_posts')
 ]
--- vehicle/views.py
+++ vehicle/views.py
@@ -9,21 +9,3 @@ class Home(generic.TemplateView):
     '''
     template_name='index.html'
 
-def search(request):
-    '''
-    search results
-    '''
-    # Get all vehicle data
-    queryset = Vehicle.objects.all()
-
-    if request.POST:
-        searched = request.POST["searched"]
-        results = Vehicle.objects.filter(Q(title__contains=searched) |
-                                     Q(content__contains=searched)
-                                    ).distinct()
-        context = {'queryset': queryset}
-
-        return render(request, 'vehicle/search.html', {'results': results, 'searched': searched})
-    else:
-        return render(request, 'vehicle/search.html', context)
-
