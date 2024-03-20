eb31f503b41a2f46212d2ed4df5abccf37e3aaed
mehmet durmus <mdurmus@outlook.com>
2024-03-20 13:24:22 +0000

    added listing vehicles in a  category metot

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -4,6 +4,5 @@ from django.urls import path
 urlpatterns=[
     path('', Home.as_view(), name='home'),
     path('categories/', CategoryListView.as_view(), name='categories'),
-    path('category/<int:category_id>/', ListVehicleInCategoryListView.as_view(), name='category_vehicles'),
-    
+    path('category/<int:category_id>/', ListVehicleInCategoryListView.as_view(), name='category_vehicles'), 
 ]
--- vehicle/views.py
+++ vehicle/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic, View 
 from django.views.generic import ListView
 from .models import *
+
 class Home(generic.TemplateView):
     '''
     Home page view
@@ -12,25 +13,6 @@ class Home(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all().order_by('title')
         return context
-
-class ListVehicleInCategoryListView(ListView):
-    '''
-    Category page view
-    '''
-
-    template_name='vehicle/category_vehicles.html'
-    model = Category
-    context_object_name = 'categories'
-    paginate_by = 5
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['page_name'] = "Category"
-        return context
-
-    # data filtering.
-    def get_queryset(self):
-        return Category.objects.all()
 
 class CategoryListView(ListView):
     template_name='vehicle/categories.html'
@@ -46,3 +28,22 @@ class CategoryListView(ListView):
     # data filtering.
     def get_queryset(self):
         return Category.objects.all()
+
+class ListVehicleInCategoryListView(ListView):
+    '''
+    List all vehicle in a category
+    '''
+
+    template_name='vehicle/category_vehicles.html'
+    model = Vehicle
+    context_object_name = 'vehicles'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_name'] = "Vehicles"
+        return context
+
+    def get_queryset(self):
+        category_id = self.kwargs['category_id']
+        return Vehicle.objects.filter(category_id=category_id)
