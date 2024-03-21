3f77eeb66b34fa87622730972860d038cab57ef7
mehmet durmus <mdurmus@outlook.com>
2024-03-21 15:39:28 +0000

    added comment form and url structure chenged

--- vehicle/forms.py
+++ vehicle/forms.py
@@ -9,4 +9,4 @@ class CommentForm(forms.ModelForm):
     """
     class Meta:
         model = Comment
-        fields = ('comment')
+        fields = ('comment',)
--- vehicle/urls.py
+++ vehicle/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 
 urlpatterns=[
     path('', Home.as_view(), name='home'),
-    path('<slug:slug>/', VehicleDetail.as_view(), name='vehicle_detail'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('categories/', CategoryListView.as_view(), name='categories'),
     path('category/<str:cat_name>/', ListVehicleInCategoryListView.as_view(), name='category_vehicles'), 
+    path('<slug:slug>/', VehicleDetail.as_view(), name='vehicle_detail'),
 ]
