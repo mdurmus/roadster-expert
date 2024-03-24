79eab15c169bde1f20f23ad72d4075dc5a1b2c3d
mehmet durmus <mdurmus@outlook.com>
2024-03-24 10:44:33 +0000

    Old js  codes have been deleted and message module has been added.

--- roadster_project/settings.py
+++ roadster_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
@@ -63,6 +64,14 @@ SITE_ID=1
 
 LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG:'alert-info',
+    messages.INFO:'alert-info',
+    messages.SUCCESS:'alert-success',
+    messages.WARNING:'alert-warning',
+    messages.ERROR:'alert-danger'
+}
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
