14a24a19bcb2e9ac15e9fa4086e13faec578b50e
mehmet durmus <mdurmus@outlook.com>
2024-03-31 14:32:36 +0000

    js files validated

--- static/js/roadster.js
+++ static/js/roadster.js
@@ -10,18 +10,3 @@ $(document).ready(function(){
     });
 
 })
-
-//Change Navbar color and background on scroll
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(document).scrollTop() > 20) {
-            $(".navbar").addClass("nav-active");
-            $(".nav-link").addClass("nav-active");
-            $(".navbar-brand img").addClass("nav-active");
-        } else {
-            $(".navbar").removeClass("nav-active");
-            $(".nav-link").removeClass("nav-active");
-            $(".navbar-brand img").removeClass("nav-active");
-        }
-    });
-});
