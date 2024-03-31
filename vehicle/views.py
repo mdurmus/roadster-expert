0cc84c7a24c3f7c17d1eedc4d68dfd02dee776e1
mehmet durmus <mdurmus@outlook.com>
2024-03-31 11:00:42 +0000

    views.py files edited for pep8 in vehicle app

--- vehicle/views.py
+++ vehicle/views.py
@@ -1,28 +1,33 @@
 from django.contrib.messages.views import SuccessMessageMixin
-from .forms import CommentForm, ProfileUpdateForm, UserUpdateForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import (
-    render, get_object_or_404, reverse, redirect, resolve_url)
-from django.views import generic, View 
-from django.views.generic import ListView, UpdateView
-from .models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import HttpResponseRedirect
 from django.db.models import Q
+from django.http import HttpResponseRedirect
+from django.shortcuts import (render,
+                              get_object_or_404,
+                              reverse,
+                              redirect,
+                              resolve_url)
+from django.views import generic, View
+from django.views.generic import ListView, UpdateView
+from .forms import CommentForm, ProfileUpdateForm, UserUpdateForm
+from .models import *
+
 
 class Home(generic.TemplateView):
     '''
     Home page view
     '''
-    template_name='index.html'
+    template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
 
+
 class CategoryListView(ListView):
-    template_name='vehicle/categories.html'
+    template_name = 'vehicle/categories.html'
     model = Category
     context_object_name = 'categories'
     paginate_by = 4
@@ -32,16 +37,16 @@ class CategoryListView(ListView):
         context['page_name'] = "Category"
         return context
 
-    # data filtering.
     def get_queryset(self):
         return Category.objects.all()
+
 
 class ListVehicleInCategoryListView(ListView):
     '''
     List all vehicle in a category
     '''
 
-    template_name='vehicle/category_vehicles.html'
+    template_name = 'vehicle/category_vehicles.html'
     model = Vehicle
     context_object_name = 'vehicles'
     paginate_by = 6
@@ -56,19 +61,22 @@ class ListVehicleInCategoryListView(ListView):
         category = Category.objects.get(title=title)
         return Vehicle.objects.filter(category=category)
 
+
 def about(request):
     '''
     About page
     '''
-    context = {'page_name':'About Us'}
-    return render(request, 'vehicle/about.html',context)
+    context = {'page_name': 'About Us'}
+    return render(request, 'vehicle/about.html', context)
+
 
 def contact(request):
     '''
     Contact page
     '''
-    context = {'page_name':'Contact Us'}
-    return render(request, 'vehicle/contact.html',context)
+    context = {'page_name': 'Contact Us'}
+    return render(request, 'vehicle/contact.html', context)
+
 
 class VehicleDetail(View):
     '''
@@ -80,12 +88,14 @@ class VehicleDetail(View):
         print(slug)
         queryset = Vehicle.objects.filter(status=1)
         vehicle = get_object_or_404(queryset, slug=slug)
-        comments = Comment.objects.filter(vehicle=vehicle, approved=True).order_by("-created_on")
+        comments = Comment.objects.filter(vehicle=vehicle,
+                                          approved=True).order_by(
+                                              "-created_on")
         comments_count = Comment.objects.filter(vehicle=vehicle).count()
         liked = False
         if vehicle.likes.filter(id=request.user.id).exists():
             liked = True
-        
+
         return render(
             request,
             "vehicle/vehicle_detail.html",
@@ -99,11 +109,12 @@ class VehicleDetail(View):
                 "page_name": vehicle.title,
             },
         )
-    
+
     def post(self, request, slug, *args, **kwargs):
         queryset = Vehicle.objects.filter(status=1)
         vehicle = get_object_or_404(queryset, slug=slug)
-        comments = vehicle.vehicle_comments.filter(approved=True).order_by("-created_on")
+        comments = vehicle.vehicle_comments.filter(approved=True).order_by(
+            "-created_on")
         comments_count = Comment.objects.filter(vehicle=vehicle).count()
         liked = False
         if vehicle.likes.filter(id=self.request.user.id).exists():
@@ -135,6 +146,7 @@ class VehicleDetail(View):
             },
         )
 
+
 class EditComment(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     """
     Edit comment
@@ -144,9 +156,9 @@ class EditComment(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     form_class = CommentForm
     success_message = 'The comment was successfully updated'
 
-
     def get_success_url(self):
-       return reverse('vehicle-detail', args=[self.object.vehicle.slug])
+        return reverse('vehicle-detail', args=[self.object.vehicle.slug])
+
 
 @login_required
 def delete_comment(request, comment_id):
@@ -157,11 +169,13 @@ def delete_comment(request, comment_id):
     comment.delete()
     messages.success(request, 'The comment was deleted successfully')
     return HttpResponseRedirect(reverse(
-        'vehicle-detail', args=[comment.vehicle.slug]))
+                                        'vehicle-detail',
+                                        args=[comment.vehicle.slug]))
+
 
 class PostLike(View):
     '''
-    For like a vehicle 
+    For like a vehicle
     '''
     def post(self, request, slug):
         vehicle = get_object_or_404(Vehicle, slug=slug)
@@ -171,30 +185,35 @@ class PostLike(View):
             messages.warning(request, 'Unliked!')
         else:
             vehicle.likes.add(request.user)
-            messages.success(request,'Liked')
+            messages.success(request, 'Liked')
 
         return HttpResponseRedirect(reverse('vehicle-detail', args=[slug]))
+
 
 @login_required
 def profile(request):
     if request.method == 'POST':
         user_form = UserUpdateForm(request.POST, instance=request.user)
-        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance = request.user.profile)
+        profile_form = ProfileUpdateForm(
+                                         request.POST,
+                                         request.FILES,
+                                         instance=request.user.profile)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
             messages.success(request, 'Your account has been updated!')
             return redirect(reverse('my-profile'))
     else:
-        user_form = UserUpdateForm(instance = request.user)
-        profile_form = ProfileUpdateForm(instance = request.user.profile)
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
-        'user_form':user_form,
-        'profile_form':profile_form,
-        'page_name':'Your profile'
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'page_name': 'Your profile'
     }
-    return render(request, 'vehicle/profile.html',context,)
+    return render(request, 'vehicle/profile.html', context)
+
 
 def searchform(request, **kwargs):
     """
@@ -202,15 +221,18 @@ def searchform(request, **kwargs):
     """
     if request.method == 'POST':
         search_query = request.POST.get('search_query', '')
-        search_result = Vehicle.objects.filter(Q(title__icontains = search_query) | 
-                                               Q(summary__icontains = search_query))
+        search_result = Vehicle.objects
+        .filter(
+                Q(title__icontains=search_query) |
+                Q(summary__icontains=search_query)
+                )
 
         context = {
-            'search_query':search_query,
+            'search_query': search_query,
             'search_results': search_result
         }
 
         return render(request, 'vehicle/search.html', context)
-    
+
     elif request.method == 'GET':
         return render(request, 'vehicle/search.html')
