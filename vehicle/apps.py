a8826ba0bfb9bee9074d3cd3d061fd9531819ff9
mehmet durmus <mdurmus@outlook.com>
2024-04-02 08:40:07 +0000

    added signal file

--- vehicle/apps.py
+++ vehicle/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class VehicleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'vehicle'
+
+    def ready(self):
+        """
+        Boardcast user's signal to app
+        """
+        import vehicle.signals
