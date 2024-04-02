5977fd7d66058589e55598c10336993919c8dec6
mehmet durmus <mdurmus@outlook.com>
2024-04-02 15:09:56 +0000

    deployment commit

--- roadster_project/settings.py
+++ roadster_project/settings.py
@@ -31,7 +31,9 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-mdurmus-roadsterexpert-uq91yxq6suu.ws-eu110.gitpod.io',
                  '.herokuapp.com']
